8e0fcd648990ca843354fc2dbb9d10227a656d4c
Maie Palmeos <maie.palmeos@gmail.com>
2019-10-28 21:52:51 +0200

    Day 3 solution

--- CSharpStuff/MonthOfCSharp/Program.cs
+++ CSharpStuff/MonthOfCSharp/Program.cs
@@ -6,7 +6,30 @@ namespace MonthOfCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = int.Parse(Console.ReadLine());
+
+            PrintWeird(N);
+        }
+
+        public static void PrintWeird(int n)
+        {
+            if (n%2 == 0)
+            {
+                if (2 <= n && n <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                } else if (6 <= n && n <= 20)
+                {
+                    Console.WriteLine("Weird");
+                } else if (n > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                
+            } else
+            {
+                Console.WriteLine("Weird");
+            }
         }
     }
 }
