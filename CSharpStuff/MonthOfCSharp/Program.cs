60596576f1c6b4a9450479419088cdf17155e88c
Maie Palmeos <maie.palmeos@gmail.com>
2019-10-28 21:14:09 +0200

    Initial creation of 30 Days of Coding Project

---
+++ CSharpStuff/MonthOfCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonthOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
