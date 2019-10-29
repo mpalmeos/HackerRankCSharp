d85d18e8b30549722894bc32ec2ada55158b4ac3
Maie Palmeos <maie.palmeos@gmail.com>
2019-10-29 21:47:43 +0200

    Day 4 task done

--- CSharpStuff/MonthOfCSharp/Program.cs
+++ CSharpStuff/MonthOfCSharp/Program.cs
@@ -6,13 +6,15 @@ namespace MonthOfCSharp
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-
-            PrintWeird(N);
+            
         }
 
+        //Day 3
         public static void PrintWeird(int n)
         {
+            //int N = int.Parse(Console.ReadLine());
+            //PrintWeird(N);
+            
             if (n%2 == 0)
             {
                 if (2 <= n && n <= 5)
@@ -30,6 +32,41 @@ namespace MonthOfCSharp
             {
                 Console.WriteLine("Weird");
             }
+        }
+        
+        //Day 2 Challenge
+        static int solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            /*double meal_cost = 12.00;
+            int tip_percent = 20;
+            int tax_percent = 8;
+            Console.WriteLine(solve(meal_cost, tip_percent, tax_percent));*/
+            
+            double tip = meal_cost * tip_percent / 100;
+            double tax = meal_cost*tax_percent/100;
+            double total = meal_cost + tip + tax;
+            return Convert.ToInt32(total);
+
+        }
+        
+        //Day 1 Challenge
+        static int simpleArraySum(int[] ar)
+        {
+            /*
+                    * Write your code here.
+                    */
+            int sum = 0;
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
+
+            return sum;
+
+            //int[] ar = {1,2,3,4,10,11};
+            //int suma = simpleArraySum(ar)
+            //Console.WriteLine(suma);
         }
     }
 }
